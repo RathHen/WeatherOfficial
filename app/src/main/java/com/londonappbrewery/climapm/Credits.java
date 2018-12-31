978cb880457287e4f26fd67eb9c33070cbe36190
RathHen <42045764+RathHen@users.noreply.github.com>
2018-12-31 15:19:22 -0600

    Credits Page

---
+++ app/src/main/java/com/londonappbrewery/climapm/Credits.java
@@ -0,0 +1,30 @@
+package com.londonappbrewery.climapm;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+
+
+public class Credits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.credits);
+
+        Button returnButton = findViewById(R.id.return_button);
+
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+    }
+}
